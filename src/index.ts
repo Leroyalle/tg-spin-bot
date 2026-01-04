c62a4230453c5b7194b7e2dfd7ec6227f3a2cc0d
Leroyalle <nikolajmelonov@yandex.ru>
2026-01-04 23:22:06 +0300

    feat(app): add scripts and setup bot

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
+import 'dotenv/config';
 import express from 'express';
+import { Telegraf } from 'telegraf';
 
 const app = express();
+const bot = new Telegraf(process.env.BOT_API_TOKEN as string);
+
+bot.start(ctx => {
+  ctx.reply('🎰 Привет! Жми /spin, чтобы крутить рулетку');
+});
+
+bot.command('spin', ctx => {
+  ctx.reply('Рулетка крутится...');
+});
 
 app.get('/', (_, res) => res.send('ok'));
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
 });
+
+bot.launch();
